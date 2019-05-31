daaedc44e6846d25abd229474c40b96dcc2ecfae
Corina Wong <hello@corinawong.com>
2019-05-31 11:06:53 -0400

    Set up

--- src/presentation.js
+++ src/presentation.js
@@ -1,94 +1,130 @@
 // Import React
 import React from 'react';
+import styled from 'react-emotion';
 
 // Import Spectacle Core tags
-import {
-  BlockQuote,
-  Cite,
-  Deck,
-  Heading,
-  ListItem,
-  List,
-  Quote,
-  Slide,
-  Text,
-} from 'spectacle';
+import { Deck, Heading, ListItem, List, Appear, Slide, Image } from 'spectacle';
 
 // Import theme
 import createTheme from 'spectacle/lib/themes/default';
 
-// Require CSS
 require('normalize.css');
 
 const theme = createTheme(
   {
-    primary: 'white',
-    secondary: '#1F2022',
-    tertiary: '#03A9FC',
+    primary: '#0d1016',
+    secondary: '#FFEDFF',
+    tertiary: '#fcc303',
     quaternary: '#CECECE',
   },
   {
-    primary: 'Montserrat',
+    primary: 'Noto Serif',
     secondary: 'Helvetica',
   }
 );
+const images = {
+  title: require('./images/title.png'),
+  titlecat: require('./images/titlecat.png'),
+  cats: require('./images/giphy.webp'),
+  objliteral: require('./images/objLiteral.png'),
+};
+
+const CustomListItem = styled(ListItem)`
+  font-size: 0.75em;
+  margin: 1em;
+  min-width: 16em;
+  line-height: 1.2;
+`;
+
+//////////////////////////////////////////
+const CustomSlide = styled(Slide)`
+  margin-top: -6em;
+`;
+
+const TitleGif = styled(Image)`
+  position: absolute;
+  top: 60%;
+  right: 43%;
+`;
+
+const SlideTitle = styled(Heading)`
+  text-align: center;
+  font-size: 30px;
+  font-weight: normal;
+  font-style: italic;
+`;
 
 export default class Presentation extends React.Component {
   render() {
     return (
       <Deck
-        transition={['zoom', 'slide']}
+        transition={['zoom']}
         transitionDuration={500}
         theme={theme}
-      >
-        <Slide transition={['zoom']} bgColor="primary">
-          <Heading size={1} fit caps lineHeight={1} textColor="secondary">
-            Spectacle Boilerplate
-          </Heading>
-          <Text margin="10px 0 0" textColor="tertiary" size={1} fit bold>
-            open the presentation/index.js file to get started
-          </Text>
+        //////////////////////////////////////////
+        contentWidth='2000'
+        bgColor='primary'>
+        <Slide transition={['']} transitionDuration={0}>
+          <div style={{ postion: 'relative' }}>
+            <Image src={images.title} />
+          </div>
         </Slide>
-        <Slide transition={['fade']} bgColor="tertiary">
-          <Heading size={6} textColor="primary" caps>
-            Typography
-          </Heading>
-          <Heading size={1} textColor="secondary">
-            Heading 1
-          </Heading>
-          <Heading size={2} textColor="secondary">
-            Heading 2
-          </Heading>
-          <Heading size={3} textColor="secondary">
-            Heading 3
-          </Heading>
-          <Heading size={4} textColor="secondary">
-            Heading 4
-          </Heading>
-          <Heading size={5} textColor="secondary">
-            Heading 5
-          </Heading>
-          <Text size={6} textColor="secondary">
-            Standard text
-          </Text>
+        <Slide transition={['']}>
+          <div style={{ position: 'relative' }}>
+            <Image src={images.titlecat} />
+
+            <TitleGif src={images.cats} />
+          </div>
         </Slide>
-        <Slide transition={['fade']} bgColor="primary" textColor="tertiary">
-          <Heading size={6} textColor="secondary" caps>
-            Standard List
-          </Heading>
-          <List>
-            <ListItem>Item 1</ListItem>
-            <ListItem>Item 2</ListItem>
-            <ListItem>Item 3</ListItem>
-            <ListItem>Item 4</ListItem>
-          </List>
-        </Slide>
-        <Slide transition={['fade']} bgColor="secondary" textColor="primary">
-          <BlockQuote>
-            <Quote>Example Quote</Quote>
-            <Cite>Author</Cite>
-          </BlockQuote>
-        </Slide>
+        <CustomSlide>
+          <SlideTitle textColor='secondary'>Object Literals</SlideTitle>
+          <div style={{ display: 'flex' }}>
+            <Appear>
+              <Image src={images.objliteral} />
+            </Appear>
+
+            <List>
+              <Appear>
+                <CustomListItem>
+                  <strong>
+                    <em>Encapsulation</em>
+                  </strong>{' '}
+                  - related data and functionality together
+                </CustomListItem>
+              </Appear>
+              <Appear>
+                <CustomListItem>
+                  What if you need to make more of these objects?
+                </CustomListItem>
+              </Appear>
+              <Appear>
+                <CustomListItem>Breaks DRY principle</CustomListItem>
+              </Appear>
+              <Appear>
+                <CustomListItem>Error-prone</CustomListItem>
+              </Appear>
+              <Appear>
+                <CustomListItem>
+                  What if you need to change one of the methods?
+                </CustomListItem>
+              </Appear>
+              <Appear>
+                <CustomListItem>
+                  What if you need to add / remove methods for all?
+                </CustomListItem>
+              </Appear>
+              <Appear>
+                <CustomListItem>
+                  Copies of identical functions — bad for memory
+                </CustomListItem>
+              </Appear>
+            </List>
+          </div>
+        </CustomSlide>
+        <CustomSlide>
+          <SlideTitle textColor='secondary'>Factory Functions</SlideTitle>
+          {/* <div style={{ display: 'flex' }} /> */}
+        </CustomSlide>
       </Deck>
     );
   }
