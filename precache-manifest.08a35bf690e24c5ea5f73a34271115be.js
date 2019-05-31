eacfc98766c08e2d6318f93eb34dea254db6c676
Corina Wong <hello@corinawong.com>
2019-05-31 11:31:03 -0400

    Updates

---
+++ precache-manifest.08a35bf690e24c5ea5f73a34271115be.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "e2815eb5df36aaf5f4b9abce30c88a2b",
+    "url": "/creating-objects/static/media/titlecat.e2815eb5.png"
+  },
+  {
+    "revision": "22e2ee335432516ca05c0df62fdfabc4",
+    "url": "/creating-objects/static/media/title.22e2ee33.png"
+  },
+  {
+    "revision": "40e8aa2027da46d03f7d33f96586d7cc",
+    "url": "/creating-objects/static/media/objLiteral.40e8aa20.png"
+  },
+  {
+    "revision": "da6439e360963aa85724cf2c93797e0d",
+    "url": "/creating-objects/static/media/giphy.da6439e3.webp"
+  },
+  {
+    "revision": "7b4744c567d469b080b2",
+    "url": "/creating-objects/static/js/runtime~main.7b4744c5.js"
+  },
+  {
+    "revision": "5b2cd6479cc5789e92bf",
+    "url": "/creating-objects/static/js/main.5b2cd647.chunk.js"
+  },
+  {
+    "revision": "b229e58a0d7db16bbae9",
+    "url": "/creating-objects/static/js/1.b229e58a.chunk.js"
+  },
+  {
+    "revision": "b229e58a0d7db16bbae9",
+    "url": "/creating-objects/static/css/1.e9ab1fb3.chunk.css"
+  },
+  {
+    "revision": "8447e392c4e54f82a3801c748a14915b",
+    "url": "/creating-objects/index.html"
+  }
+];
