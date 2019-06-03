4f546049fd7a44f89fc81f28e8e41a2d23763e81
Corina Wong <hello@corinawong.com>
2019-06-03 14:30:46 -0400

    Update slides

--- src/presentation.js
+++ src/presentation.js
@@ -9,7 +9,6 @@ import {
   ListItem,
   Layout,
   List,
-  Notes,
   Appear,
   Text,
   Slide,
@@ -23,7 +22,7 @@ require('normalize.css');
 
 const theme = createTheme(
   {
-    primary: '#0d1016',
+    primary: '#0a0e14',
     secondary: '#FFEDFF',
     tertiary: '#fcc303',
     quaternary: '#CECECE',
@@ -56,31 +55,22 @@ const images = {
   fin: require('./images/fin.png'),
 };
 
-const CustomList = styled(List)`
-  list-style-position: outside;
-`;
-
 const CustomListItem = styled(ListItem)`
-  font-size: 1.5rem;
+  font-size: 1.25rem;
   margin: 2.5rem;
   line-height: 1.45;
-`;
-
-//////////////////////////////////////////
-const CustomSlide = styled(Slide)`
-  margin-top: -10em;
 `;
 
 const TitleGif = styled(Image)`
   position: absolute;
   top: 60%;
-  right: 43%;
+  right: 42.5%;
 `;
 
 const SlideTitle = styled(Heading)`
-  font-size: 22px;
+  font-size: 18px;
   font-style: italic;
-  margin: 1em;
+  margin: 0 0 2em;
 `;
 
 const Keyword = styled(Text)`
@@ -113,45 +103,52 @@ export default class Presentation extends React.Component {
             <TitleGif src={images.cats} />
           </div>
         </Slide>
-        <CustomSlide>
+        <Slide>
           <SlideTitle textColor='secondary'>Object Literals</SlideTitle>
           <Layout>
             <Appear>
               <Image src={images.objliteral} />
             </Appear>
-            <Appear>
-              <CustomList>
+
+            <List>
+              <Appear>
                 <CustomListItem>
                   <strong>
                     <em>Encapsulation</em>
                   </strong>{' '}
                   — related data and functionality together
                 </CustomListItem>
-
+              </Appear>
+              <Appear>
                 <CustomListItem>
                   What if you need to make more of these objects?
                 </CustomListItem>
-
+              </Appear>
+              <Appear>
                 <CustomListItem>Breaks DRY principle</CustomListItem>
-
+              </Appear>
+              <Appear>
                 <CustomListItem>Error-prone</CustomListItem>
-
+              </Appear>
+              <Appear>
                 <CustomListItem>
                   What if you need to change one of the methods?
                 </CustomListItem>
-
+              </Appear>
+              <Appear>
                 <CustomListItem>
                   What if you need to add / remove methods for all?
                 </CustomListItem>
-
+              </Appear>
+              <Appear>
                 <CustomListItem>
                   Copies of identical functions — bad for memory
                 </CustomListItem>
-              </CustomList>
-            </Appear>
+              </Appear>
+            </List>
           </Layout>
-        </CustomSlide>
-        <CustomSlide>
+        </Slide>
+        <Slide>
           <SlideTitle textColor='secondary'>Factory Functions</SlideTitle>
           <Layout>
             <Appear>
@@ -162,42 +159,49 @@ export default class Presentation extends React.Component {
               <Image src={images.factoryFunction2} />
             </Appear>
           </Layout>
-        </CustomSlide>
-        <CustomSlide transition={['']} transitionDuration={0}>
+        </Slide>
+        <Slide transition={['']} transitionDuration={0}>
           <SlideTitle textColor='secondary'>Factory Functions</SlideTitle>
           <Layout>
             <Image src={images.factoryFunction1} />
             <Image src={images.factoryFunction3} />
           </Layout>
-        </CustomSlide>
-        <CustomSlide transition={['']} transitionDuration={0}>
+        </Slide>
+        <Slide transition={['']} transitionDuration={0}>
           <SlideTitle textColor='secondary'>Factory Functions</SlideTitle>
           <Layout>
             <Image src={images.factoryFunction1} />
             <Image src={images.factoryFunction4} />
           </Layout>
-        </CustomSlide>
-        <CustomSlide>
+        </Slide>
+        <Slide>
           <SlideTitle textColor='secondary'>
             The Prototypal Nature of JavaScript
           </SlideTitle>
-          <Appear>
-            <div style={{ display: 'flex', flexDirection: 'column' }}>
-              <Image src={images.proto} />
 
-              <CustomList
-                style={{
-                  maxWidth: '40em',
-                  margin: '0 auto',
-                  paddingLeft: '1em',
-                }}>
+          <div style={{ display: 'flex', flexDirection: 'column' }}>
+            <Image src={images.proto} />
+
+            <List
+              style={{
+                maxWidth: '31em',
+                paddingLeft: '2em',
+                margin: '0 auto',
+              }}>
+              <Appear>
                 <CustomListItem>
                   All functions and all arrays are{' '}
                   <em>
                     <strong>also objects</strong>
                   </em>
-                  . All objects have a hidden <Keyword>__proto__</Keyword> property
                 </CustomListItem>
+              </Appear>
+              <Appear>
+                <CustomListItem>
+                  All objects have a hidden <Keyword>__proto__</Keyword> property
+                </CustomListItem>
+              </Appear>
+              <Appear>
                 <CustomListItem>
                   The <Keyword>__proto__</Keyword> default for objects created using
                   an object literal is <Keyword>Object.prototype</Keyword>. The
@@ -205,19 +209,22 @@ export default class Presentation extends React.Component {
                   <Keyword>Array.prototype</Keyword> and for functions it is
                   <Keyword>Function.prototype</Keyword>
                 </CustomListItem>
-
+              </Appear>
+              <Appear>
                 <CustomListItem>
                   <Keyword>__proto__</Keyword> provides access to bonus functionality
                   — if JavaScript doesn't find a method on an object, it checks the
                   object's <Keyword>__proto__</Keyword>
                 </CustomListItem>
-              </CustomList>
-            </div>
-          </Appear>
-        </CustomSlide>
-        <CustomSlide transition={['']} transitionDuration={0}>
-          <SlideTitle textColor='secondary'>Object.create()</SlideTitle>
-          <div style={{ display: 'flex', justifyContent: 'space-around' }}>
+              </Appear>
+            </List>
+          </div>
+        </Slide>
+        <Slide transition={['']} transitionDuration={0}>
+          <SlideTitle textColor='secondary'>
+            <Keyword>Object.create()</Keyword>
+          </SlideTitle>
+          <div style={{ display: 'flex', justifyContent: 'space-evenly' }}>
             <div>
               <Appear>
                 <Image src={images.objectCreate} />
@@ -228,13 +235,15 @@ export default class Presentation extends React.Component {
               <Appear>
                 <Image src={images.objectCreate2} />
               </Appear>
-              <Appear>
-                <CustomList style={{ maxWidth: '25em' }}>
+
+              <List style={{ maxWidth: '23em', paddingLeft: '1em' }}>
+                <Appear>
                   <CustomListItem>
                     Use <Keyword>Object.create()</Keyword> to override the default{' '}
                     <Keyword>__proto__</Keyword> reference
                   </CustomListItem>
-
+                </Appear>
+                <Appear>
                   <CustomListItem>
                     A new empty object is created, with its{' '}
                     <Keyword>__proto__</Keyword> set to{' '}
@@ -242,29 +251,32 @@ export default class Presentation extends React.Component {
                       <strong>catMethods</strong>
                     </em>
                   </CustomListItem>
-
+                </Appear>
+                <Appear>
                   <CustomListItem>
                     The new object's <Keyword>__proto__</Keyword> is itself an
                     object, which is why it has its <em>own</em>{' '}
                     <Keyword>__proto__</Keyword> that references the default{' '}
                     <Keyword>Object.prototype</Keyword>
                   </CustomListItem>
-
+                </Appear>
+                <Appear>
                   <CustomListItem>
                     This is how <Keyword>Array.prototype</Keyword> and
                     <Keyword>Function.prototype</Keyword> reference back to
                     <Keyword>Object.prototype</Keyword>
                   </CustomListItem>
-
+                </Appear>
+                <Appear>
                   <CustomListItem>
                     Look-up process on the "prototype chain"
                   </CustomListItem>
-                </CustomList>
-              </Appear>
+                </Appear>
+              </List>
             </div>
           </div>
-        </CustomSlide>
-        <CustomSlide>
+        </Slide>
+        <Slide>
           <SlideTitle textColor='secondary'>
             The <Keyword>prototype</Keyword> Property
           </SlideTitle>
@@ -272,10 +284,10 @@ export default class Presentation extends React.Component {
             <Appear>
               <Image src={images.prototype1} />
             </Appear>
-            <CustomList
+            <List
               style={{
-                maxWidth: '17em',
-                margin: '0 auto',
+                maxWidth: '16em',
+                margin: '4em auto 0',
               }}>
               <Appear>
                 <CustomListItem>Functions are also objects</CustomListItem>
@@ -290,14 +302,14 @@ export default class Presentation extends React.Component {
                 <CustomListItem>
                   Use a function's <Keyword>prototype</Keyword> property (which is an
                   object) to store shared functions that will be "inherited"
-                  (behaviour delegation) by objects created by that function, using{' '}
-                  <Keyword>new</Keyword>
+                  (behaviour delegation) by objects that were created by that
+                  function using the <Keyword>new</Keyword> keyword
                 </CustomListItem>
               </Appear>
               <Appear>
                 <CustomListItem>
                   So let's rewrite our function to set the <em>name</em> and{' '}
-                  <em>age</em> data, and also use the function's{' '}
+                  <em>age</em> parameters, and also use the function's{' '}
                   <Keyword>prototype</Keyword> object to store shared functions
                   (instead of putting them in a separate one such as{' '}
                   <em>
@@ -306,10 +318,10 @@ export default class Presentation extends React.Component {
                   )
                 </CustomListItem>
               </Appear>
-            </CustomList>
+            </List>
           </Layout>
-        </CustomSlide>
-        <CustomSlide>
+        </Slide>
+        <Slide>
           <SlideTitle textColor='secondary'>
             Constructor Functions & <Keyword>new</Keyword> Keyword
           </SlideTitle>
@@ -321,12 +333,15 @@ export default class Presentation extends React.Component {
               <Appear>
                 <Image src={images.new2} />
               </Appear>
-              <Appear>
-                <CustomList
-                  style={{
-                    maxWidth: '27em',
-                    margin: '0 auto',
-                  }}>
+
+              <List
+                style={{
+                  maxWidth: '24em',
+                  paddingLeft: '1em',
+                  margin: '0 auto',
+                }}>
+                {' '}
+                <Appear>
                   <CustomListItem>
                     Functions that require{' '}
                     <Keyword>
@@ -334,7 +349,8 @@ export default class Presentation extends React.Component {
                     </Keyword>{' '}
                     to run are called Constructor Functions
                   </CustomListItem>
-
+                </Appear>
+                <Appear>
                   <CustomListItem>
                     Constructor functions are capitalized by convention to let other
                     devs know it needs{' '}
@@ -343,61 +359,66 @@ export default class Presentation extends React.Component {
                     </Keyword>{' '}
                     to run properly
                   </CustomListItem>
-
+                </Appear>
+                <Appear>
                   <CustomListItem>
                     <Keyword>
                       <strong>new</strong>
                     </Keyword>
                     does 3 things for you:
-                    <CustomList ordered>
-                      <CustomListItem>
-                        Create an{' '}
-                        <strong>
-                          <em>empty object</em>
-                        </strong>{' '}
-                        with an implicit <Keyword>this</Keyword> assignment
-                      </CustomListItem>
-
-                      <CustomListItem>
-                        Set the <Keyword>__proto__</Keyword> in the created object to
-                        reference the constructor function's{' '}
-                        <Keyword>prototype</Keyword> property
-                      </CustomListItem>
-
-                      <CustomListItem>
-                        <Keyword>return</Keyword> out the created object
-                      </CustomListItem>
-                    </CustomList>
+                    <List ordered>
+                      <Appear>
+                        <CustomListItem>
+                          Create an{' '}
+                          <strong>
+                            <em>empty object</em>
+                          </strong>{' '}
+                          with an implicit <Keyword>this</Keyword> assignment
+                        </CustomListItem>
+                      </Appear>
+                      <Appear>
+                        <CustomListItem>
+                          Set the <Keyword>__proto__</Keyword> in the created object
+                          to reference the constructor function's{' '}
+                          <Keyword>prototype</Keyword> property
+                        </CustomListItem>
+                      </Appear>
+                      <Appear>
+                        <CustomListItem>
+                          <Keyword>return</Keyword> out the created object
+                        </CustomListItem>
+                      </Appear>
+                    </List>
                   </CustomListItem>
-                </CustomList>
-              </Appear>
+                </Appear>
+              </List>
             </div>
           </Layout>
-        </CustomSlide>
+        </Slide>
 
-        <CustomSlide>
+        <Slide>
           <SlideTitle textColor='secondary'>
             ES6 <Keyword>class</Keyword> Keyword
           </SlideTitle>
           <Appear>
             <Image src={images.class} />
           </Appear>
-        </CustomSlide>
+        </Slide>
 
-        <CustomSlide transition={['']} transitionDuration={0}>
+        <Slide transition={['']} transitionDuration={0}>
           <SlideTitle textColor='secondary'>
             ES6 <Keyword>class</Keyword> Keyword
           </SlideTitle>
           <Image src={images.class2} />
-        </CustomSlide>
+        </Slide>
 
-        <CustomSlide>
+        <Slide>
           <SlideTitle textColor='secondary'>Bonus: Subclassing</SlideTitle>
           <Appear>
             <Image src={images.subclass} />
           </Appear>
-        </CustomSlide>
-        <CustomSlide>
+        </Slide>
+        <Slide>
           <SlideTitle textColor='secondary'>Bonus: Subclassing</SlideTitle>
 
           <Image src={images.subclass2} />
@@ -408,12 +429,12 @@ export default class Presentation extends React.Component {
             rel='noopener noreferrer'>
             <Image src={images.subclass3} />
           </a>
-        </CustomSlide>
+        </Slide>
 
-        <CustomSlide>
+        <Slide>
           <SlideTitle textColor='secondary'>Fin — Questions?</SlideTitle>
           <Image src={images.fin} />
-        </CustomSlide>
+        </Slide>
       </Deck>
     );
   }
